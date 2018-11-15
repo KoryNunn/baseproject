96818157a04649bdbec9b394a099ba68ed7bcddf
korynunn <knunn187@gmail.com>
2018-11-16 00:52:48 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,24 @@
+# Base project for quickly starting new sites.
+
+[baseproject.korynunn.com](https://baseproject.korynunn.com)
+
+# Tools:
+
+## Common:
+
+Async/flow control: [righto](https://www.npmjs.com/package/righto) Not in use in this app because it's so basic but I use it alllll the time.
+
+## Server:
+
+Routing: [sea-lion](https://www.npmjs.com/package/sea-lion)
+File serving: [dion](https://www.npmjs.com/package/dion)
+Config loading: [config](https://www.npmjs.com/package/config)
+
+## Client:
+
+Component data-binding: [fastn](https://www.npmjs.com/package/fastn)
+Validation: [blazon](https://www.npmjs.com/package/blazon)
+
+# Size
+
+Bundled/min/gzipped the client is 43Kb and includes a full-featured toolset to build most things.
---
+++ client/index.js
@@ -0,0 +1,53 @@
+var fastn = require('./fastn');
+var blazon = require('blazon');
+var { Maybe, Cast } = blazon;
+
+var model = new fastn.Model({
+    errors: {}
+});
+
+var maybeNumber = blazon(Maybe(Cast(Number), 0));
+
+function setFromEvent(errorModel, type, event){
+    var fieldName = event.target.previousSibling.textContent;
+    try {
+        this.value(type(event.target.value));
+        errorModel.remove('error');
+    } catch (error) {
+        errorModel.set('error', error.message.match(/(Expected .*?),/)[1]);
+    }
+}
+
+function validation(type, callback){
+    var errorModel = new fastn.Model();
+    var fieldLabel;
+
+    var errorField = fastn('div', { class: 'error' }, fastn.binding('error')).attach(errorModel);
+
+    function setValue(event){
+        setFromEvent.call(this, errorModel, type, event);
+    }
+
+    return callback(setValue, errorField);
+}
+
+var ui = fastn('div', { class: 'content' },
+    fastn('span', { class: 'madeBy' }, 'Made by', fastn('a', { href: 'https://korynunn.com' }, 'Kory Nunn')),
+    fastn('h1', { class: 'pageHeading' }, 'An evolving no-frills base project so i can build stuff quickly'),
+    fastn('section',
+        fastn('h2', 'Intended for quick prototypes. Not a "best practice" example.')
+    ),
+    validation(maybeNumber, (set, errorField) => fastn('field',
+        fastn('label', 'Example field with validation'),
+        fastn('input', {
+            placeholder: 'Type numbers',
+            value: fastn.binding('number')
+        })
+        .on('input', set),
+        errorField
+    ))
+)
+.attach(model)
+.render();
+
+window.addEventListener('load', () => document.body.appendChild(ui.element));
